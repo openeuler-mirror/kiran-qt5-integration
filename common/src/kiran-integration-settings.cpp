71223161f8c103846079ce5987ecce6a59f04a62
liuxinhao <liuxinhao@kylinsec.com.cn>
2023-06-05 15:40:41 +0800

    feat(style app): Adjust the topic configuration to whitelist
    - 调整主题配置为白名单，默认包括部分需要支持的kiran桌面环境进程名

--- common/src/kiran-integration-settings.cpp
+++ common/src/kiran-integration-settings.cpp
@@ -4,6 +4,7 @@
 #include <QMutexLocker>
 #include <QScopedPointer>
 #include <QSettings>
+#include <QRegExp>
 
 KiranIntegrationSettings::KiranIntegrationSettings()
 {
@@ -18,8 +19,8 @@ KiranIntegrationSettings::~KiranIntegrationSettings()
 void KiranIntegrationSettings::init()
 {
     QSettings settings("/etc/kiran-qt5-integration/kiran-qt5-integration.ini",QSettings::IniFormat);
-    QStringList disableStyleApps = settings.value("Style/disable-kiran-style-apps","").toStringList();
-    m_disableKiranStyleApps.append(disableStyleApps);
+    QStringList kiranStyleApps = settings.value("Style/kiran-style-apps","").toStringList();
+    m_kiranStyleApps.unite(kiranStyleApps.toSet());
 }
 
 KiranIntegrationSettings* KiranIntegrationSettings::instance()
@@ -39,7 +40,24 @@ KiranIntegrationSettings* KiranIntegrationSettings::instance()
     return pInst.data();
 }
 
-QStringList KiranIntegrationSettings::getDisableKiranStyleApps()
+QSet<QString> KiranIntegrationSettings::getKiranStyleApps()
 {
-    return m_disableKiranStyleApps;
+    return m_kiranStyleApps;
+}
+
+bool KiranIntegrationSettings::appKiranStyleAvailable(const QString& app)
+{
+    bool matched = false;
+    QRegExp regExp("",Qt::CaseSensitive,QRegExp::Wildcard);
+
+    auto set = KiranIntegrationSettings::instance()->getKiranStyleApps();
+    foreach(auto pattern,set)
+    {
+        regExp.setPattern(pattern);
+        matched = regExp.exactMatch(app);
+        if( matched )
+            break;
+    }
+
+    return matched;
 }
